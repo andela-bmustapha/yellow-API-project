0b0cb811cad46521df4e8939db476bddc8e338cb
toystars <toystars2008@gmail.com>
2015-05-08 14:35:06 +0000

    Refactored the js file to use Object Literal

--- js/yellowAPI.js
+++ js/yellowAPI.js
@@ -1,35 +1,44 @@
-$(document).ready(function() {
-	$("#search-button").click(function() {
-    
-    var result = $("#result-container");
-    result.fadeToggle('slow');
-    result.empty();
-		var userSearch = $(".search-input").val();
-		var yellowUrl = 'http://api.sandbox.yellowapi.com/FindBusiness/?what=' + userSearch + '&where=Canada&fmt=JSON&pgLen=5&apikey=ttnecms7z552km87egfshs2e&UID=127.0.0.1';
-		
-    $.getJSON( yellowUrl, function( data ) {
-			result.append("<ol>");
-			$.each(data.listings, function(index, element) {
-				var addressArray = [];
-				var busName = "<li>Business Name: ";
-				var busAdd = "<li>Business Address: ";
-				busName += element.name + '</li>';
-				if (typeof element.address == 'object') {
-					$.each(element.address, function(ind, addressField) {
-						addressArray.push(addressField);
-					});
-				busAdd += addressArray + '</li>';
-				}
+var yellowApi = {
 
-			result.append(busName);
-			result.append(busAdd);
-			result.append("<br />");
-			result.append("<br />");
-      result.append("<hr />");		
-			});
-  	});
+  result: $("#result-container"),  // defines the result container
 
-  		result.append("</ol>");	
-      result.fadeIn('slow');
-	});
+  // define method
+  showContent: function() {
+    var userSearch = document.getElementById('search-input').value;
+    var yellowUrl = 'http://api.sandbox.yellowapi.com/FindBusiness/?what=' + userSearch + '&where=Canada&fmt=JSON&pgLen=5&apikey=ttnecms7z552km87egfshs2e&UID=127.0.0.1'
+
+    yellowApi.result.fadeToggle('slow');
+    yellowApi.result.empty();
+
+    $.getJSON( yellowUrl, function(data) {
+      yellowApi.result.append("<ol>");
+      $.each(data.listings, function(index, element) {
+        var addressArray = [];
+        var busName = "<li>Business Name: ";
+        var busAdd = "<li>Business Address: ";
+        busName += element.name + '</li>';
+        if (typeof element.address == 'object') {
+          $.each(element.address, function(ind, addressField) {
+            addressArray.push(addressField);
+          });
+          busAdd += addressArray + '</li>';
+        }
+
+        yellowApi.result.append(busName);
+        yellowApi.result.append(busAdd);
+        yellowApi.result.append("<br />");
+        yellowApi.result.append("<br />");
+        yellowApi.result.append("<hr />");    
+      });
+    });
+    yellowApi.result.append("</ol>"); 
+    yellowApi.result.fadeIn('slow');
+  }
+};
+
+// attach an event hadler to the button
+document.getElementById('search-button').addEventListener('click', function(e) {
+  e.preventDefault();
+  yellowApi.showContent();
 });
+
